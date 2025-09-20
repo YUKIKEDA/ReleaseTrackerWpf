bc44ea089201160e328d2965dee23d68b1ea86eb
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-09-20 10:54:45 +0900

    feat: DiffViewModelから差分カウント機能を削除し、DiffViewのUIを簡素化しました。差分表示のためのツリー比較に焦点を当て、関連するプロパティとメソッドを整理しました。

--- ReleaseTrackerWpf/ViewModels/DiffViewModel.cs
+++ ReleaseTrackerWpf/ViewModels/DiffViewModel.cs
@@ -11,9 +11,6 @@ namespace ReleaseTrackerWpf.ViewModels
     {
         private ObservableCollection<TreeNodeViewModel> _oldTree = new();
         private ObservableCollection<TreeNodeViewModel> _newTree = new();
-        private int _addedCount;
-        private int _deletedCount;
-        private int _modifiedCount;
         private string _oldSnapshotName = string.Empty;
         private string _newSnapshotName = string.Empty;
 
@@ -57,35 +54,6 @@ namespace ReleaseTrackerWpf.ViewModels
             }
         }
 
-        public int AddedCount
-        {
-            get => _addedCount;
-            private set
-            {
-                _addedCount = value;
-                OnPropertyChanged(nameof(AddedCount));
-            }
-        }
-
-        public int DeletedCount
-        {
-            get => _deletedCount;
-            private set
-            {
-                _deletedCount = value;
-                OnPropertyChanged(nameof(DeletedCount));
-            }
-        }
-
-        public int ModifiedCount
-        {
-            get => _modifiedCount;
-            private set
-            {
-                _modifiedCount = value;
-                OnPropertyChanged(nameof(ModifiedCount));
-            }
-        }
 
         public void LoadComparison(DirectorySnapshot? oldSnapshot, DirectorySnapshot? newSnapshot)
         {
@@ -103,9 +71,6 @@ namespace ReleaseTrackerWpf.ViewModels
             // Build trees (the difference highlighting is already applied during MarkDifferences)
             OldTree = TreeNodeViewModel.BuildTreeFromSnapshot(oldSnapshot);
             NewTree = TreeNodeViewModel.BuildTreeFromSnapshot(newSnapshot);
-
-            // Update counts
-            UpdateCounts();
         }
 
         private Dictionary<string, FileItem> CreateFlatItemDictionary(List<FileItem>? items)
@@ -173,32 +138,6 @@ namespace ReleaseTrackerWpf.ViewModels
         }
 
 
-        private void UpdateCounts()
-        {
-            AddedCount = CountNodesByType(NewTree, DifferenceType.Added);
-            DeletedCount = CountNodesByType(OldTree, DifferenceType.Deleted);
-            ModifiedCount = CountNodesByType(NewTree, DifferenceType.Modified);
-        }
-
-        private int CountNodesByType(ObservableCollection<TreeNodeViewModel> tree, DifferenceType type)
-        {
-            int count = 0;
-            foreach (var node in tree)
-            {
-                count += CountNodesByTypeRecursive(node, type);
-            }
-            return count;
-        }
-
-        private int CountNodesByTypeRecursive(TreeNodeViewModel node, DifferenceType type)
-        {
-            int count = node.DifferenceType == type ? 1 : 0;
-            foreach (var child in node.Children)
-            {
-                count += CountNodesByTypeRecursive(child, type);
-            }
-            return count;
-        }
 
         private static string FormatFileSize(long size)
         {
