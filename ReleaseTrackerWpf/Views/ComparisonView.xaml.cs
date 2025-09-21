0fe289f97a5e5e3b4f5f1ad8c71facc6becfddbe
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-09-21 14:50:13 +0900

    feat: ComparisonViewにスクロール機能を追加し、Shiftキーによる横スクロールを実装しました。これにより、比較結果の表示がより快適になりました。

--- ReleaseTrackerWpf/Views/ComparisonView.xaml.cs
+++ ReleaseTrackerWpf/Views/ComparisonView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ReleaseTrackerWpf.Views
 {
@@ -10,6 +11,29 @@ namespace ReleaseTrackerWpf.Views
         public ComparisonView()
         {
             InitializeComponent();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (sender is ScrollViewer scrollViewer)
+            {
+                // Shiftキーが押されている場合は横スクロール
+                if (Keyboard.Modifiers == ModifierKeys.Shift)
+                {
+                    if (e.Delta > 0)
+                    {
+                        // 上方向のスクロール = 左スクロール
+                        scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset - 50);
+                    }
+                    else
+                    {
+                        // 下方向のスクロール = 右スクロール
+                        scrollViewer.ScrollToHorizontalOffset(scrollViewer.HorizontalOffset + 50);
+                    }
+                    e.Handled = true;
+                }
+                // Shiftキーが押されていない場合は通常の縦スクロール（デフォルト動作）
+            }
         }
     }
 }
