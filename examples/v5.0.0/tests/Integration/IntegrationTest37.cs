126f00e14f6f929c61719648eb2d4443fec1f781
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-10-02 21:25:20 +0900

    feat: 大規模なExampleデータを追加

---
+++ examples/v5.0.0/tests/Integration/IntegrationTest37.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tests.Integration
+{
+    public class IntegrationTest37
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        
+        public virtual void Validate()
+        {
+            if (string.IsNullOrEmpty(Name))
+                throw new ArgumentException("Name is required");
+        }
+        
+        public virtual async Task<bool> ProcessAsync()
+        {
+            await Task.Delay(100);
+            return true;
+        }
+    }
+}
